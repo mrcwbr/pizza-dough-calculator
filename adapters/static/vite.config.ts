e34eb7477a41f2230a71fb983500dc62cda61b15
Marco Weber <marco.weber-mm@t-online.de>
2024-07-03 15:41:55 +0200

    fix: origin

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -12,7 +12,7 @@ export default extendConfig(baseConfig, () => {
     },
     plugins: [
       staticAdapter({
-        origin: "https://mrcwbr.github.io/pizza-dough-calculator/",
+        origin: "https://mrcwbr.github.io/pizza-dough-calculator",
       }),
     ],
   };
